329e0d0c364fc45e5bbb49cf26fa820b38467022
yogeshrkatole@gmail.com <yogeshrkatole@gmail.com>
2024-12-19 13:27:07 +0530

    Maintain Dictionary of Person-city & Person-state

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,26 +1,54 @@
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class AddressBook {
 	private List<ContactAddress> contactAddresses;
+	private Map<String, List<ContactAddress>> cityDictionary;
+    private Map<String, List<ContactAddress>> stateDictionary;
+
 
 	public AddressBook() {
 		this.contactAddresses = new ArrayList<>();
+		this.cityDictionary = new HashMap<>();
+        this.stateDictionary = new HashMap<>();
 	}
 
 	public void addContactAddress(ContactAddress contactAddress) {
         if (contactAddresses.stream()
                 .anyMatch(existingContact -> existingContact.equals(contactAddress))) {
-            System.out.println("Duplicate contact.A person with this name already exists in the Address Book.");
+            System.out.println("Duplicate contact. A person with this name already exists in the Address Book.");
         } else {
             contactAddresses.add(contactAddress);
+            updateDictionaries(contactAddress);
             System.out.println("Contact added successfully!");
         }
     }
 
+	private void updateDictionaries(ContactAddress contactAddress) {
+	    List<ContactAddress> cityContacts = cityDictionary.get(contactAddress.getCity());
+	    if (cityContacts == null) {
+	        cityContacts = new ArrayList<>();
+	        cityDictionary.put(contactAddress.getCity(), cityContacts);
+	    }
+	    cityContacts.add(contactAddress);
+
+	    List<ContactAddress> stateContacts = stateDictionary.get(contactAddress.getState());
+	    if (stateContacts == null) {
+	        stateContacts = new ArrayList<>();
+	        stateDictionary.put(contactAddress.getState(), stateContacts);
+	    }
+	    stateContacts.add(contactAddress);
+	}
+	
+	private void removeFromDictionaries(ContactAddress contactAddress) {
+        cityDictionary.getOrDefault(contactAddress.getCity(), new ArrayList<>()).remove(contactAddress);
+        stateDictionary.getOrDefault(contactAddress.getState(), new ArrayList<>()).remove(contactAddress);
+    }
+	
 	public void showContactAddresses() {
 		if (!contactAddresses.isEmpty()) {
 
